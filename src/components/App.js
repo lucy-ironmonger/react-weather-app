0c74306efda1369204d322e3897860bcd41a4157
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2021-02-05 22:19:00 +0000

    setup props between app and location details

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "../styles/App.css";
+import PropTypes from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+const App = ({ location }) => {
+  const { city, country } = location;
   return (
     <div className="App">
-      <h1>Weather App!</h1>
+      <LocationDetails city={city} country={country} />
     </div>
   );
-}
+};
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
+import { location } from "./data/forecast.json";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App location={location} />
   </React.StrictMode>,
   document.getElementById("root")
 );
