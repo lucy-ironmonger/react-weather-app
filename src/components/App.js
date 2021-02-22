e8b25884bc4be69319bba685479eced925fc0a16
Lucy Ironmonger <37961811+ironmongrrrl@users.noreply.github.com>
2021-02-22 06:40:28 +0000

    starting to connect the API (not working)

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+/* eslint react/prop-types: 0 */
+/* eslint-disable no-unused-vars */
+
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
+import axios from "axios";
 import LocationDetails from "./LocationDetails"; // First component
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 
-const App = ({ location, forecasts }) => {
-  const { city, country } = location;
+const App = () => {
+  const [selectedDate, setSelectedDate] = useState(0);
 
-  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  // based on the data, location is {} and forecast is []
+  // we are setting the initial values.
+  const [location, setLocation] = useState({ city: "", country: "" });
+  const [forecasts, setForecasts] = useState([]);
+
+  const getForecast = () => {
+    const endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
+
+    axios.get(endpoint).then((response) => {
+      setSelectedDate(response.data.forecasts[0].date);
+      setLocation(response.data.location);
+      setForecasts(response.data.forecasts);
+    });
+  };
+
+  useEffect(() => {
+    getForecast();
+  }, []);
+
   // The below is setting the selectedForecast to be the object in forecasts that equals the selectedDate
   const selectedForecast = forecasts.find(
     (forecast) => selectedDate === forecast.date
@@ -19,34 +41,36 @@ const App = ({ location, forecasts }) => {
     setSelectedDate(date);
   };
 
+  // conditional logic added with selectedForecast&& so it only makes component if there is one
+
   return (
     <div className="weather-app">
-      <LocationDetails city={city} country={country} />
+      <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
       />
-      <ForecastDetails forecast={selectedForecast} />
+      {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
     </div>
   );
 };
 
-App.propTypes = {
-  forecasts: PropTypes.arrayOf(
-    PropTypes.shape({
-      date: PropTypes.number,
-      description: PropTypes.string,
-      icon: PropTypes.number,
-      temperature: PropTypes.shape({
-        max: PropTypes.number,
-        min: PropTypes.number,
-      }),
-    })
-  ).isRequired,
-  location: PropTypes.shape({
-    city: PropTypes.string,
-    country: PropTypes.string,
-  }).isRequired,
-};
+// App.propTypes = {
+//   forecasts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       date: PropTypes.number,
+//       description: PropTypes.string,
+//       icon: PropTypes.number,
+//       temperature: PropTypes.shape({
+//         max: PropTypes.number,
+//         min: PropTypes.number,
+//       }),
+//     })
+//   ).isRequired,
+//   location: PropTypes.shape({
+//     city: PropTypes.string,
+//     country: PropTypes.string,
+//   }).isRequired,
+// };
 
 export default App;
--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -5,7 +5,7 @@ import WeatherIcon from "react-icons-weather";
 import moment from "moment";
 
 const ForecastSummary = (props) => {
-  const { date, description, icon, temperature, onForecastSelect } = props;
+  const { date, description, icon, temperature, onSelect } = props;
   return (
     <div className="forecast-summary">
       <div className="forecast-summary__date">
@@ -20,7 +20,7 @@ const ForecastSummary = (props) => {
         ${temperature.max}\u00b0c`}
       </div>
       <div className="forecast-summaries__button">
-        <button type="button" onClick={() => onForecastSelect(date)}>
+        <button type="button" onClick={() => onSelect(date)}>
           More info
         </button>
       </div>
@@ -36,7 +36,7 @@ ForecastSummary.propTypes = {
     max: PropTypes.number,
     min: PropTypes.number,
   }).isRequired,
-  onForecastSelect: PropTypes.func.isRequired,
+  onSelect: PropTypes.func.isRequired,
 };
 
 export default ForecastSummary;
